91bdc6cddb6b03c2ba36ad81f5324e40aab77974
doumaarmand@gmail.com <doumaarmand@gmail.com>
2024-10-23 17:05:42 +0000

    modif

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -4,6 +4,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <title>Groupe Mvolais</title>
+    <link rel="icon" href="{{ asset('assets/images/logo2.jpeg') }}" type="image/jpeg" />
+    <title>Groupe M'VOLAIS</title>
     @vite(['resources/scss/app.scss','resources/css/app.css','resources/js/app.js'])
 </head>
